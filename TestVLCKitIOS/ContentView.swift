e72fe51aa61998c05ea27180d5e234afaf8223b7
Denis Blondeau <dbappdev@outlook.com>
2023-02-26 15:34:58 -0500

    Initial Commit

---
+++ TestVLCKitIOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestVLCKitIOS
+//
+//  Created by Denis Blondeau on 2023-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestVLCKitIOS/TestVLCKitIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestVLCKitIOSApp.swift
+//  TestVLCKitIOS
+//
+//  Created by Denis Blondeau on 2023-02-26.
+//
+
+import SwiftUI
+
+@main
+struct TestVLCKitIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
